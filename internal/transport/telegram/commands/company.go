3f1fb8417a6b72c06803560a5017ea14a3c9eff1
Dmitry Gridnev <dmitry.gridnev@testit.software>
2023-08-16 15:57:39 +0200

    Fixed get company command

--- internal/transport/telegram/commands/company.go
+++ internal/transport/telegram/commands/company.go
@@ -51,7 +51,7 @@ func (c *CompanyCommands) GetMyCompanies(m *tgbotapi.Message) (tgbotapi.MessageC
 		*Name:*  _%s_ 
 		*Email:* _%s_
 		*Token:* _%s_
-		`, company.Name, strings.Replace(company.Email, ".", "\\.", 1), company.Token)
+		`, company.Name, strings.Replace(company.Email, ".", "\\.", -1), company.Token)
 
 	if len(company.ChatIds) > 0 {
 		msg.Text += "\n*Chats:*"
