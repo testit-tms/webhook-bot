3a78e192212e3e47cf63ac1cd3a06966af5c7c2b
Dmitry Gridnev <dmitriy.gridnev@outlook.com>
2023-08-29 10:07:09 +0300

    Apply migrations from code (#6)
    * Apply migrations from code
    
    ---------
    
    Co-authored-by: Dmitry Gridnev <dmitry.gridnev@testit.software>

--- pkg/database/postgres.go
+++ pkg/database/postgres.go
@@ -1,15 +1,24 @@
 package database
 
 import (
+	"embed"
 	"fmt"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+	"github.com/pressly/goose/v3"
+)
+
+//go:embed migrations/*.sql
+var embedMigrations embed.FS
+
+const (
+	driverName = "postgres"
 )
 
 func Initialize(host string, port int64, user, password, dbname string) (*sqlx.DB, error) {
 	const op = "database.Initialize"
-	db, err := sqlx.Connect("postgres", fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+	db, err := sqlx.Connect(driverName, fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname))
 	if err != nil {
 		return nil, fmt.Errorf("%s: cannot connect to database: %w", op, err)
@@ -17,6 +26,16 @@ func Initialize(host string, port int64, user, password, dbname string) (*sqlx.D
 
 	if err = db.Ping(); err != nil {
 		return nil, fmt.Errorf("%s: cannot ping database: %w", op, err)
+	}
+
+	goose.SetBaseFS(embedMigrations)
+
+	if err := goose.SetDialect(driverName); err != nil {
+		return nil, fmt.Errorf("%s: cannot set goose dialect: %w", op, err)
+	}
+
+	if err := goose.Up(db.DB, "migrations"); err != nil {
+		return nil, fmt.Errorf("%s: cannot run goose migrations: %w", op, err)
 	}
 
 	return db, nil
